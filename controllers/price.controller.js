447788cbb7eea8d1de764c84bda5ee2b991040bf
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2024-10-17 22:18:19 +0700

    udate code

--- controllers/price.controller.js
+++ controllers/price.controller.js
@@ -1,200 +1,205 @@
-const { Op } = require("sequelize/lib/sequelize");
-const { PriceHeader, PriceLine, Station } = require("../models");
+const {Op} = require("sequelize/lib/sequelize");
+const {PriceHeader, PriceLine, Station} = require("../models");
 
 const createPriceHeader = async (req, res) => {
-    const data = req.body;
-    try {
-        const newRate = await PriceHeader.create({
-            ...data,
-        });
-        res.status(200).send(newRate);
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error);
-    }
+	const data = req.body;
+	try {
+		const newRate = await PriceHeader.create({
+			...data,
+		});
+		res.status(200).send(newRate);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 const createPriceLine = async (req, res) => {
-    const data = req.body;
-    try {
-        const newRate = await PriceLine.create({
-            ...data,
-        });
-        res.status(200).send(newRate);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+	const data = req.body;
+	try {
+		const newRate = await PriceLine.create({
+			...data,
+		});
+		res.status(200).send(newRate);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 };
 
 const getAllPriceHeader = async (req, res) => {
-    try {
-        const RateList = await PriceHeader.findAll();
+	try {
+		const RateList = await PriceHeader.findAll();
 
-        res.status(200).send(RateList);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+		res.status(200).send(RateList.length > 0 ? RateList : []);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 };
 
 const getOnePriceHeader = async (req, res) => {
-    try {
-        const result = await PriceHeader.findOne({
-            where: {
-                id: req.params.idHeader
-            },
-            include: [
-                {
-                    model: PriceLine,
-                    as: "priceLine",
-                },
-            ]
-        });
+	try {
+		const result = await PriceHeader.findOne({
+			where: {
+				id: req.params.idHeader,
+			},
+			include: [
+				{
+					model: PriceLine,
+					as: "priceLine",
+					include: [
+						{
+							model: Station,
+							as: "fromStationFK",
+						},
+						{
+							model: Station,
+							as: "toStationFK",
+						},
+					],
+				},
+			],
+		});
 
-        res.status(200).send(result);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+		res.status(200).send(result ? result : null);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 };
 
 const getOnePriceLine = async (req, res) => {
-    try {
-        const result = await PriceLine.findOne({
-            where: { id: req.params.idLine },
-            include: [
-                {
-                    model: Station,
-                    as: "fromStationFK",
-                },
-                {
-                    model: Station,
-                    as: "toStationFK",
-                },
-            ],
-        });
+	try {
+		const result = await PriceLine.findOne({
+			where: {id: req.params.idLine},
+			include: [
+				{
+					model: Station,
+					as: "fromStationFK",
+				},
+				{
+					model: Station,
+					as: "toStationFK",
+				},
+			],
+		});
 
-        res.status(200).send(result);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+		res.status(200).send(result ? result : null);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 };
 
 const getAllPriceLine = async (req, res) => {
-    try {
-        const RateList = await PriceLine.findAll({
+	try {
+		const RateList = await PriceLine.findAll({
+			where: {priceHeaderId: req.params.idHeader},
+			include: [
+				{
+					model: Station,
+					as: "fromStationFK",
+				},
+				{
+					model: Station,
+					as: "toStationFK",
+				},
+			],
+		});
 
-            where: { priceHeaderId: req.params.idHeader },
-            include: [
-                {
-                    model: Station,
-                    as: "fromStationFK",
-                },
-                {
-                    model: Station,
-                    as: "toStationFK",
-                },
-            ],
-        });
-
-        res.status(200).send(RateList);
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error);
-    }
+		res.status(200).send(RateList.length > 0 ? RateList : []);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
-
 const updatePriceHeader = async (req, res) => {
-    const data = req.body;
-    try {
+	const data = req.body;
+	try {
+		if (req.body.status) {
+			const price = await PriceHeader.findOne({
+				where: {id: req.params.id},
+			});
 
-        if (req.body.status) {
-            const price = await PriceHeader.findOne({
-                where: { id: req.params.id }
-            })
+			const priceHeaders = await PriceHeader.findAll({
+				where: {
+					endDate: {[Op.gte]: new Date(price.startDate)},
+					status: true,
+					id: {[Op.notIn]: [req.params.id]},
+				},
+			});
 
-            const priceHeaders = await PriceHeader.findAll({
-                where: {
-                    endDate: { [Op.gte]: new Date(price.startDate) },
-                    status: true,
-                    id: { [Op.notIn]: [req.params.id] }
-                }
-            })
+			if (priceHeaders.length > 0) {
+				return res.status(400).send({message: "Can not update status"});
+			}
+		}
 
-            if (priceHeaders.length > 0) {
-                return res.status(400).send({ message: "Can not update status" })
-            }
-        }
+		const newRate = await PriceHeader.update(
+			{
+				...data,
+			},
+			{
+				where: {id: req.params.id},
+			}
+		);
 
-        const newRate = await PriceHeader.update(
-            {
-                ...data,
-            },
-            {
-                where: { id: req.params.id }
-            });
-
-        res.status(200).send(newRate);
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error);
-    }
+		res.status(200).send(newRate);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 const updatePriceLine = async (req, res) => {
-    const data = req.body;
-    try {
-        const newRate = await PriceLine.update(
-            {
-                ...data,
-            },
-            {
-                where: { id: req.params.id }
-            });
+	const data = req.body;
+	try {
+		const newRate = await PriceLine.update(
+			{
+				...data,
+			},
+			{
+				where: {id: req.params.id},
+			}
+		);
 
-        res.status(200).send(newRate);
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error);
-    }
+		res.status(200).send(newRate);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
-
 
 const deletePriceLine = async (req, res) => {
-    try {
-        await PriceLine.destroy(
-            {
-                where: { id: req.params.id }
-            });
+	try {
+		await PriceLine.destroy({
+			where: {id: req.params.id},
+		});
 
-        res.status(200).send("ok");
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error);
-    }
+		res.status(200).send("ok");
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
-
 
 const deletePriceHeader = async (req, res) => {
-    try {
-        await PriceHeader.destroy(
-            {
-                where: { id: req.params.id }
-            });
+	try {
+		await PriceHeader.destroy({
+			where: {id: req.params.id},
+		});
 
-        res.status(200).send("ok");
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error);
-    }
+		res.status(200).send("ok");
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 module.exports = {
-    createPriceHeader,
-    createPriceLine,
-    getAllPriceHeader,
-    getAllPriceLine,
-    getOnePriceLine,
-    getOnePriceHeader,
-    updatePriceHeader,
-    updatePriceLine,
-    deletePriceHeader,
-    deletePriceLine
+	createPriceHeader,
+	createPriceLine,
+	getAllPriceHeader,
+	getAllPriceLine,
+	getOnePriceLine,
+	getOnePriceHeader,
+	updatePriceHeader,
+	updatePriceLine,
+	deletePriceHeader,
+	deletePriceLine,
 };
--- controllers/promotion.controller.js
+++ controllers/promotion.controller.js
@@ -1,5 +1,5 @@
-const { PromotionHeader, PromotionLine, PromotionDetail, Trip } = require("../models");
-const { Op, where } = require("sequelize");
+const {PromotionHeader, PromotionLine, PromotionDetail, Trip} = require("../models");
+const {Op, where} = require("sequelize");
 // header
 const createPromotionHeader = async (req, res) => {
 	const data = req.body;
@@ -22,7 +22,7 @@ const updatePromotionHeader = async (req, res) => {
 				...data,
 			},
 			{
-				where: { id: req.params.id },
+				where: {id: req.params.id},
 			}
 		);
 
@@ -53,7 +53,7 @@ const getOnePromotionHeader = async (req, res) => {
 			],
 		});
 
-		res.status(200).send(result);
+		res.status(200).send(result ? result : null);
 	} catch (error) {
 		res.status(500).send(error);
 	}
@@ -63,7 +63,7 @@ const getAllPromotionHeader = async (req, res) => {
 	try {
 		const RateList = await PromotionHeader.findAll();
 
-		res.status(200).send(RateList);
+		res.status(200).send(RateList.length > 0 ? RateList : []);
 	} catch (error) {
 		console.log(error);
 		res.status(500).send(error);
@@ -73,7 +73,7 @@ const getAllPromotionHeader = async (req, res) => {
 const deletePromotionHeader = async (req, res) => {
 	try {
 		await PromotionHeader.destroy({
-			where: { id: req.params.id },
+			where: {id: req.params.id},
 		});
 
 		res.status(200).send("ok");
@@ -81,8 +81,6 @@ const deletePromotionHeader = async (req, res) => {
 		console.log(error);
 		res.status(500).send(error);
 	}
-
-
 };
 
 const createPromotionLine = async (req, res) => {
@@ -95,7 +93,7 @@ const createPromotionLine = async (req, res) => {
 			status: data.status,
 			description: data.description,
 			promotionHeaderId: data.promotionHeaderId,
-			promotionType: data.promotionType
+			promotionType: data.promotionType,
 		});
 
 		const promotionDetail = await PromotionDetail.create({
@@ -107,16 +105,14 @@ const createPromotionLine = async (req, res) => {
 			budget: data.budget,
 			promotionHeaderId: data.promotionHeaderId,
 			promotionLineId: newRate.id,
-			budgetUsed: 0
-		})
+			budgetUsed: 0,
+		});
 
 		res.status(200).send(promotionDetail);
 	} catch (error) {
 		console.log(error);
 		res.status(500).send(error);
 	}
-
-
 };
 
 const getOnePromotionLine = async (req, res) => {
@@ -133,7 +129,7 @@ const getOnePromotionLine = async (req, res) => {
 			],
 		});
 
-		res.status(200).send(result);
+		res.status(200).send(result ? result : null);
 	} catch (error) {
 		res.status(500).send(error);
 	}
@@ -141,61 +137,61 @@ const getOnePromotionLine = async (req, res) => {
 
 const getAllPromotionLine = async (req, res) => {
 	try {
+		const result = await PromotionLine.findAll({
+			where: {promotionHeaderId: req.params.headerId},
 
-		const result = await PromotionLine.findAll(
-			{
-				where: { promotionHeaderId: req.params.headerId },
+			include: [
+				{
+					model: PromotionDetail,
+					as: "promotionDetail",
+				},
+			],
+		});
 
-				include: [
-					{
-						model: PromotionDetail,
-						as: "promotionDetail",
-					},
-				],
-			});
-
-		res.status(200).send(result);
+		res.status(200).send(result.lenght > 0 ? result : []);
 	} catch (error) {
 		res.status(500).send(error);
 	}
 };
 
-
-
 const updatePromotionLine = async (req, res) => {
 	const data = req.body;
 	try {
-		await PromotionLine.update({
-			title: data.title,
-			startDate: data.startDate,
-			endDate: data.endDate,
-			status: data.status,
-			description: data.description,
-		},
+		await PromotionLine.update(
+			{
+				title: data.title,
+				startDate: data.startDate,
+				endDate: data.endDate,
+				status: data.status,
+				description: data.description,
+			},
 
-			{ where: { id: req.params.id } });
+			{where: {id: req.params.id}}
+		);
 
-		const promotionDetail = await PromotionDetail.update({
-			percentDiscount: data.percentDiscount,
-			quantityTicket: data.quantityTicket,
-			purchaseAmount: data.purchaseAmount,
-			moneyReduced: data.moneyReduced,
-			maximumDiscount: data.maximumDiscount,
-			budget: data.budget,
-		}, { where: { promotionLineId: req.params.id } })
+		const promotionDetail = await PromotionDetail.update(
+			{
+				percentDiscount: data.percentDiscount,
+				quantityTicket: data.quantityTicket,
+				purchaseAmount: data.purchaseAmount,
+				moneyReduced: data.moneyReduced,
+				maximumDiscount: data.maximumDiscount,
+				budget: data.budget,
+			},
+			{where: {promotionLineId: req.params.id}}
+		);
 
 		res.status(200).send(promotionDetail);
 	} catch (error) {
 		console.log(error);
 		res.status(500).send(error);
 	}
-
 };
 
 const deletePromotionLine = async (req, res) => {
 	try {
 		await PromotionLine.destroy({
-			where: { id: req.params.id },
+			where: {id: req.params.id},
 		});
 
 		res.status(200).send("ok");
@@ -207,43 +203,43 @@ const deletePromotionLine = async (req, res) => {
 
 const getPromotionByTripId = async (req, res) => {
 	try {
-		var array = []
+		var array = [];
 		const trip = await Trip.findOne({
-			where: { id: req.params.tripId },
+			where: {id: req.params.tripId},
 		});
 
 		if (!trip) {
-			return res.status(200).send({ data: array });
+			return res.status(200).send({data: array});
 		}
 
 		const promotionHeader = await PromotionHeader.findOne({
 			where: {
-				startDate: { [Op.lte]: new Date(trip.startTime) },
-				endDate: { [Op.gte]: new Date(trip.startTime) },
-				status: true
-			}
-		})
+				startDate: {[Op.lte]: new Date(trip.startTime)},
+				endDate: {[Op.gte]: new Date(trip.startTime)},
+				status: true,
+			},
+		});
 
 		if (!promotionHeader) {
-			return res.status(200).send({ data: array });
+			return res.status(200).send({data: array});
 		}
 
 		array = await PromotionLine.findAll({
 			where: {
-				startDate: { [Op.lte]: new Date(trip.startTime) },
-				endDate: { [Op.gte]: new Date(trip.startTime) },
+				startDate: {[Op.lte]: new Date(trip.startTime)},
+				endDate: {[Op.gte]: new Date(trip.startTime)},
 				status: true,
-				promotionHeaderId: promotionHeader.id
+				promotionHeaderId: promotionHeader.id,
 			},
 			include: [
 				{
 					model: PromotionDetail,
 					as: "promotionDetail",
-				}
-			]
-		})
+				},
+			],
+		});
 
-		return res.status(200).send({ data: array });
+		return res.status(200).send({data: array});
 	} catch (error) {
 		console.log(error);
 		res.status(500).send(error);
@@ -253,21 +249,22 @@ const getPromotionByTripId = async (req, res) => {
 const updateBugetPromotionLine = async (req, res) => {
 	const data = req.body;
 	try {
-
 		const promotionFind = await PromotionDetail.findOne({
-			where: { promotionLineId: req.params.id }
-		})
+			where: {promotionLineId: req.params.id},
+		});
 
-		const promotionDetail = await PromotionDetail.update({
-			budgetUsed: Number(promotionFind?.budgetUsed) + Number(data.budgetUsed),
-		}, { where: { promotionLineId: req.params.id } })
+		const promotionDetail = await PromotionDetail.update(
+			{
+				budgetUsed: Number(promotionFind?.budgetUsed) + Number(data.budgetUsed),
+			},
+			{where: {promotionLineId: req.params.id}}
+		);
 
 		res.status(200).send(promotionDetail);
 	} catch (error) {
 		console.log(error);
 		res.status(500).send(error);
 	}
-
 };
 
 module.exports = {
@@ -282,5 +279,5 @@ module.exports = {
 	getAllPromotionLine,
 	deletePromotionLine,
 	getPromotionByTripId,
-	updateBugetPromotionLine
+	updateBugetPromotionLine,
 };
